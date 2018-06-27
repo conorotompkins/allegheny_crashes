747d82c90314aec060859f180b1415db384bb316
Conor Tompkins <conorotompkins@gmail.com>
2018-06-27 09:07:16 -0400

    linked to github URL for dictionary

--- scripts/02_factorize_columns.R
+++ scripts/02_factorize_columns.R
@@ -3,7 +3,7 @@ library(lubridate)
 
 df_combined_allegheny_county_crash_data_2004_2017_raw <- read_csv("https://raw.githubusercontent.com/conorotompkins/allegheny_crashes/master/data/df_combined_allegheny_county_crash_data_2004_2017_raw.csv", col_types = cols(.default = "c"), progress = FALSE)
 
-df_dictionary <- read_csv("data/df_dictionary_rebuilt.csv") %>% 
+df_dictionary <- read_csv("https://raw.githubusercontent.com/conorotompkins/allegheny_crashes/master/data/df_dictionary_rebuilt.csv") %>% 
   filter(!is.na(code))
 
 df_combined_allegheny_county_crash_data_2004_2017_raw %>% 
